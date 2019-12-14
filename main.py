357770811af5cf0bbf1968cffb565ecb109bd42d
Timo Kramer <fw-git@timokramer.de>
2019-12-14 08:24:43 +0100

    envvariable PORT added

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from os import environ
 from christmas import server
 
 LOG_LEVEL = logging.getLevelName(environ.get('LOG_LEVEL', 'WARNING'))
+PORT = environ.get('PORT', 8000)
 
 if __name__ == "__main__":
     logger = logging.getLogger()
@@ -14,4 +15,4 @@ if __name__ == "__main__":
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(LOG_LEVEL)
-    server.app.run(host="0.0.0.0", port=8000, access_log=False)
+    server.app.run(host="0.0.0.0", port=PORT, access_log=False)
