9da4729da3d2fa1a5ab480c391acdb1b1490ca7b
Timo Kramer <fw-git@timokramer.de>
2019-12-17 15:57:27 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='christmas',
     description='sending christmas cards to deputies',
-    version='0.0.1',
+    version='0.1.0',
     url='https://github.com/GermanZero-de/christmas',
     keywords=['christmas'],
     classifiers=[
